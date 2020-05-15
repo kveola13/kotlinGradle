5f448c21e017db5791d6030c7e77badee16e6020
olakv <kveola13@student.westerdals.no>
2020-05-15 21:51:34 +0200

    added SendMessageToClient task

---
+++ src/test/kotlin/com/kveola13/intro/SendMessageToClientKtTest.kt
@@ -0,0 +1,12 @@
+package com.kveola13.intro
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class SendMessageToClientKtTest {
+
+    @Test
+    fun sendMessageToClient() {
+    }
+}
