42ba6d30ae4144c7e60478dd5d40d7ad98f87452
olakv <kveola13@student.westerdals.no>
2020-05-16 15:19:48 +0200

    continued work on coffeshop

--- src/main/kotlin/com/kveola13/jba/CoffeeShop.kt
+++ src/main/kotlin/com/kveola13/jba/CoffeeShop.kt
@@ -16,8 +16,38 @@ fun coffeeShopCurrentStatus(coffeeStatus: CoffeeStatus): String{
     )
 }
 
+fun takeAction(coffeeStatus: CoffeeStatus): Int {
+    print("Write action (buy, fill, take): ")
+    val userInput = readLine()
+    if(userInput.equals("buy")) {
+        print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino: ")
+        val userPurchase = Integer.valueOf(readLine())
+        println(coffeeShopCurrentStatus(coffeeStatus))
+    }
+    if(userInput.equals("fill")){
+        println("Write how many ml of water you want to add: ")
+        val waterInput = Integer.valueOf(readLine())
+        println(coffeeShopCurrentStatus(coffeeStatus))
+
+    }
+    if(userInput.equals("take")){
+        println("I gave you ${coffeeStatus.currencyAmount}")
+        coffeeStatus.currencyAmount = 0
+        println(coffeeShopCurrentStatus(coffeeStatus))
+    }
+    if(userInput.equals("q") || userInput.equals("quit")){
+        return -1
+    }
+    else{
+        println("Please choose a correct input.")
+    }
+    return 1
+}
+
 fun main() {
     val cs = CoffeeStatus()
-    cs.waterAmount = 5000
     println(coffeeShopCurrentStatus(cs))
+    while (takeAction(cs) != -1){
+        takeAction(cs)
+    }
 }
