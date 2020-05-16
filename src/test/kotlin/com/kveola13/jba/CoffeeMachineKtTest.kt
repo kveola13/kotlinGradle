8951415975701f375e3a753bc85b0d232e2a87b7
olakv <kveola13@student.westerdals.no>
2020-05-16 14:45:19 +0200

    added some tests

--- src/test/kotlin/com/kveola13/jba/CoffeeMachineKtTest.kt
+++ src/test/kotlin/com/kveola13/jba/CoffeeMachineKtTest.kt
@@ -17,4 +17,40 @@ internal class CoffeeMachineKtTest {
         val done: List<Int> = listOf(25000, 6250, 1875)
         assertEquals(done, makeCoffeeFromIngredients(125))
     }
+
+    @Test
+    fun makeCoffeeFromDesiredAmountOfCupsTest() {
+        val done: List<Int> = listOf(300, 65, 100)
+        assertEquals("Yes. I can make that amount of coffee", makeCoffeeFromDesiredAmountOfCups(1, done))
+    }
+
+    @Test
+    fun makeCoffeeFromDesiredAmountOfCupsTest2() {
+        val done: List<Int> = listOf(500, 250, 200)
+        assertEquals("No. I can make only 2 cups of coffee", makeCoffeeFromDesiredAmountOfCups(10, done))
+    }
+
+    @Test
+    fun makeCoffeeFromDesiredAmountOfCupsTest3() {
+        val done: List<Int> = listOf(1500, 299, 300)
+        assertEquals("Yes. I can make that amount of coffee (and even 2 more than that)", makeCoffeeFromDesiredAmountOfCups(3, done))
+    }
+
+    @Test
+    fun makeCoffeeFromDesiredAmountOfCupsTest4() {
+        val done: List<Int> = listOf(0, 0, 0)
+        assertEquals("No. I can make only 0 cups of coffee", makeCoffeeFromDesiredAmountOfCups(1, done))
+    }
+
+    @Test
+    fun makeCoffeeFromDesiredAmountOfCupsTest5() {
+        val done: List<Int> = listOf(0, 0, 0)
+        assertEquals("Yes. I can make that amount of coffee", makeCoffeeFromDesiredAmountOfCups(0, done))
+    }
+
+    @Test
+    fun makeCoffeeFromDesiredAmountOfCupsTest6() {
+        val done: List<Int> = listOf(200, 50, 15)
+        assertEquals("Yes. I can make that amount of coffee (and even 1 more than that)", makeCoffeeFromDesiredAmountOfCups(0, done))
+    }
 }
